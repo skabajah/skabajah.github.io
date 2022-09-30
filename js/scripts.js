080dbaeed225bbbfcbcb7d30c5b9b71da6533b54
Shadi Kabajah <skabajah@icloud.com>
2022-09-30 12:32:24 -0400

    change font

--- js/scripts.js
+++ js/scripts.js
@@ -10,12 +10,12 @@ $("#cyan").click(function(){
 	$('body').append('<style>h2,#examples h4,a#exampleLink:hover,#footer i:hover, #signature i:hover,#closePopup:hover{color:#44aaaa;} .btn-default,.logo{background-color:#44aaaa;}.navbar-inverse .navbar-nav>li>a:hover{border-bottom: 5px solid #44aaaa;}#intro{border-bottom: solid 10px #44aaaa;}</style>').hide().fadeIn(1000);   
 	});
 $("#serif").click(function(){
-	$('body').append("<style>body,h1,h2,h3,h4,h5,h6{font-family:'Kreon';}</style>").hide().fadeIn(1000);   
+	$('body').append("<style>body,h1,h2,h3,h4,h5,h6{font-family:'Kreon',serif;}</style>").hide().fadeIn(1000);   
 	});
 $("#sans").click(function(){
-	$('body').append("<style>body,h1,h2,h3,h4,h5,h6{font-family:'Josefin Sans';}</style>").hide().fadeIn(1000);   
+	$('body').append("<style>body,h1,h2,h3,h4,h5,h6{font-family:'Josefin Sans',Tahoma, sans-serif;}</style>").hide().fadeIn(1000);   
 	});
 $("#mono").click(function(){
-	$('body').append("<style>body,h1,h2,h3,h4,h5,h6{font-family:Ubuntu Mono;}</style>").hide().fadeIn(1000);   
+	$('body').append("<style>body,h1,h2,h3,h4,h5,h6{font-family: 'Ubuntu Mono', monospace;}</style>").hide().fadeIn(1000);   
 	});
 });
